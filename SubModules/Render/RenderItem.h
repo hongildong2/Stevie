d45f2d130375a2c4a93d7dd4ef39d9cf66e48e06
hongildong2 <hongildong6987@gmail.com>
2024-11-24 20:52:22 +0900

    chore : static assert added

--- SubModules/Render/RenderItem.h
+++ SubModules/Render/RenderItem.h
@@ -12,6 +12,7 @@ template<typename T>
 FORCEINLINE RenderParam* CAST_RENDER_PARAM_PTR(T* a)
 {
 	static_assert(sizeof(T) <= sizeof(RenderParam), "GIVEN RENDER PARAMETER TOO LARGE");
+	static_assert((sizeof(T) % 16) == 0, "CONSTANT BUFFER ALLIGNMENT");
 	return reinterpret_cast<RenderParam*>(a);
 }
 
@@ -19,6 +20,7 @@ template<typename U>
 FORCEINLINE void MEMCPY_RENDER_PARAM(RenderParam* dst, U* src)
 {
 	static_assert(sizeof(U) <= sizeof(RenderParam), "GIVEN RENDER PARAMETER TOO LARGE");
+	static_assert((sizeof(U) % 16) == 0, "CONSTANT BUFFER ALLIGNMENT");
 	std::memcpy(dst, src, sizeof(U));
 }
 
